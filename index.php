1697e8d434ee68b9d75e13928a385d1cd8d89209
John Norton <here@spindlecrank.com>
2024-11-14 09:46:31 -0600

    Styling update
    Update CSS styles formatting to cleanup interface

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ function cleanupFiles($files) {
 }
 
 function convertImage($source, $destination, $format) {
-    $allowedFormats = ['jpg', 'png', 'bmp', 'gif', 'ico', 'tiff', 'webp', 'pdf'];
+    $allowedFormats = ['pdf', 'jpeg', 'jpg', 'png', 'bmp', 'gif', 'ico', 'tiff', 'webp', 'pdf'];
     $imageInfo = getimagesize($source);
 
     if (!$imageInfo) {
@@ -161,7 +161,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$file_name = $uploadedFile['name'];
 		$file_tmp = $uploadedFile['tmp_name'];
 
-		$allowed_formats = ['jpg', 'png', 'bmp', 'gif', 'ico', 'tiff', 'webp', 'pdf'];
+		$allowed_formats = ['pdf', 'jpeg', 'jpg', 'png', 'bmp', 'gif', 'ico', 'tiff', 'webp', 'pdf'];
 		$fileExtension = strtolower(pathinfo($uploadedFile['name'], PATHINFO_EXTENSION));
         if (!in_array($fileExtension, $allowed_formats)) {
 			logMessage("File type not allowed!");
@@ -201,7 +201,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $zip->close();
 
     // Construct the download button HTML in $resp
-    $resp .= '<button data-href="download.php?file=' . urlencode($zip_path) . '" id="downloadButton" class="button-class flex flex-wrap item-center p-4 bg-green-500 hover:bg-green-800 text-white rounded-md transform transition duration-500 ease-in-out hover:scale-115 w-1/2">Download ZIP file</button>';
+    $resp .= '<div class="mb-4 flex items-center justify-center"> <button data-href="download.php?file=' . urlencode($zip_path) . '" id="downloadButton" class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150">
+         <div class="animate-bounce bg-ingigo-800 dark:bg-slate-800 mr-3 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center"><svg class="w-6 h-6 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"> <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg></div> Download Zip File </button></div>';
+
+    //$resp .= '<button data-href="download.php?file=' . urlencode($zip_path) . '" id="downloadButton" class="button-class flex flex-wrap item-center p-1 mb-4 bg-green-500 hover:bg-green-800 text-white rounded-md transform transition duration-500 ease-in-out hover:scale-115 w-1/2">Download ZIP file</button>';
     ob_end_clean();
     cleanupFiles($user_files);
     logMessage("Removing users files from server!");
@@ -267,6 +270,16 @@ function convertImage($source, $destination, $format) {
         * {
             font-family: Arial, sans-serif, ui-sans-serif, ui-serif, serif;
         }
+        
+        @media (min-width: *tablet-min-width*px) and (max-width: *tablet-max-width*px) { 
+ `           body { 
+                width: 100%; /* Ensure body takes full width */
+                /* Add other tablet-specific styles if needed */
+             }
+            .content-container { /* If you have a container for your form */
+                width: 100%; 
+            }
+        }
 
         @keyframes bounce {
             0%, 100% {
@@ -278,17 +291,17 @@ function convertImage($source, $destination, $format) {
             }
         }
 
-        #waiting {
+        /* #waiting {
            display: none;
             font-size: 1.5em;
             font-weight: bold;
 			color: red;
             animation: bounce 1s infinite;
-        }
+        } */
     </style>
 </head>
-<body class="bg-gradient-to-r from-indigo-800 via-purple-700 to-pink-600 flex justify-center items-center min-h-screen">
-    <div class="w-3/4 p-6 bg-gray-600 shadow-md rounded-lg animate__animated animate__slideInRight">
+<body class="bg-gradient-to-r from-indigo-700 via-purple-500 to-blue-300 flex justify-center items-center min-h-screen w-full">
+    <div class="w-3/4 p-6 bg-gray-600 shadow-md rounded-lg animate__animated animate__slideInRight focus:scale-115">
         <!-- Title Section -->
         <h1 class="text-3xl text-2xl text-center text-white mb-4 animate__animated animate__delay-1s animate__fadeInDown">Image Format Converter</h1>
         <p class="text-l text-center text-white mb-6">Convert your images to multiple formats easily and quickly.</p>
@@ -319,22 +332,33 @@ function convertImage($source, $destination, $format) {
                     </select>
                 </div>
             </div>
-            <div id="button-div" class="flex flex-col items-center mt-8">
-                <button type="submit" id="submit" class="w-1/5 bg-blue-500 text-white font-semibold py-2 shadow rounded-md transform transition duration-500 hover:bg-blue-600 hover:scale-110">Convert</button>
+            <div id="button-div" class="flex flex-col w-full items-center mt-8">
+                <button type="submit" id="submit" class="mb-4 w-2/5 sm:w-2/5 md:w-2/5 l:w-2/5 xl:w-1/5 2xl:w-1/5 p-3 bg-blue-500 text-white font-semibold shadow rounded-md transform transition duration-500 hover:bg-blue-600 hover:scale-110">Convert</button>
             </div>
         </form>
         <!-- Working and Download Divs -->
         <div id="result" class="mt-6 flex flex-col items-center text-center w-full"><?php echo $resp; ?></div>
-        <div id="waiting" class="text-white ml-1 mr-6 mb-6 mt-6 flex flex-col items-center text-center w-full"></div>
-        <div class="mt-15 flex flex-col items-center">
-		    <p class="mt-1 text-l hover:text-green-500 text-white italic animate__animated animate__delay-3s animate__zoomInUp"><span id="powered">Proudly Powered By spindlecrank.com</span></p>
+        <div id='waiting' class="mb-4 flex flex-row w-full items-center justify-center hidden">
+            <div class="flex items-center justify-center"> 
+                <button type="button" class="inline-flex items-center p-4 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled="">
+                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Processing...
+                </button>
+            </div>
+        </div>
+        <!-- <div id="waiting" class="text-white ml-1 mr-6 mb-6 mt-6 flex flex-col items-center text-center w-full"></div> -->
+        <div class="w-full flex flex-col items-center justify-center">
+		    <p class="w-full text-center mt-4 mb-8 font-semibold text-l hover:text-green-500 text-white italic animate__animated animate__delay-3s animate__zoomInUp"><span id="powered">Proudly Powered By spindlecrank.com</span></p>
 	    </div>
     </div>
 <script>
 	document.getElementById('submit').addEventListener('click', function(e) {
    		var waitingDiv = document.getElementById('waiting');
     	var resultDiv = document.getElementById('result');
-    	waitingDiv.innerHTML = "Converting Files!";
+    	//waitingDiv.innerHTML = "Converting Files!";
     	waitingDiv.style.display = 'block'; // Display the waiting message
 
     	var checkResult = setInterval(function() {
